c3165e1e9b439911a17ea613d643843ecba8b9c4
imadedwisuryadinata <dwi.suryadinata@gmail.com>
2020-10-12 13:10:15 +0800

    handle upload files & serve files

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 import {initDatabase, initTable, insertProduct, getProduct} from './database.js'
+import { UV_FS_O_FILEMAP } from 'constants';
 
 const __dirname = path.resolve()
 
@@ -17,6 +20,9 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+//use file parser
+app.use(fileUpload())
+
 //log income request
 app.use(morgan('combined'))
 
@@ -25,6 +31,7 @@ app.use(bodyParser.urlencoded())
 
 //serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next)=>{
     res.send({success: true})
@@ -57,7 +64,19 @@ app.get('/add-product', (req, res, next) => {
 //handle form post method
 app.post('/add-product', (req, res, next) => {
     console.log('Request', req.body)
-    insertProduct(db, req.body.name, parseInt(req.body.price), '-')
+    console.log('File', req.files)
+    //const fileName = req.files.photo.name
+
+    //getfilename
+    const fileName = Date.now() + req.files.photo.name
+
+    fs.writeFile(path.join(__dirname, '/files', fileName), req.files.photo.data, (err) => {
+        if (err){
+            console.error(err)
+            return
+        }
+    })
+    insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
     
     //redirect
     res.redirect('/product')
