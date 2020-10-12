39be8dd8ec7c2bb4ebaa33c26319908e8e82063d
imadedwisuryadinata <dwi.suryadinata@gmail.com>
2020-10-12 13:23:52 +0800

    environment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express';
 import hbs from 'hbs'
 import path from 'path'
@@ -88,6 +91,7 @@ app.use((err, req, res, next) =>{
     res.send(err.message)
 })
 
-app.listen(8000, () => {
-    console.log('app listen on port 8000')
+//use port environment variable
+app.listen(process.env.PORT, () => {
+    console.log(`app listen on port ${process.env.PORT}`)
 })
